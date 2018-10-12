fc32bdcc092a2172c4999ca35881a14cdc608438
samiraakhter <samira.akhter@hotmail.com>
2018-10-12 21:08:40 +0500

    Db

---
+++ SunSD/ServiceLayer/Models/Role.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ServiceLayer.Models
+{
+    public partial class Role
+    {
+        public Role()
+        {
+            SalesManager = new HashSet<SalesManager>();
+        }
+
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+
+        public ICollection<SalesManager> SalesManager { get; set; }
+    }
+}
