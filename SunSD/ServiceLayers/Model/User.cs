ce349fc4477d9e1ea6a1d00d70aeb9a9b794f4fc
samiraakhter <samira.akhter@hotmail.com>
2019-04-19 00:35:47 +0500

    Update user

---
+++ SunSD/ServiceLayers/Model/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ServiceLayers.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+        //[Display(Name = "Role")]
+        //public int RoleId { get; set; }
+
+        //[ForeignKey("RoleId")]
+        //public virtual Role Role { get; set; }
+        
+        //public string ContactNo { get; set; }
+        //public string EmailAddress { get; set; }
+        //public string ResidentialAddress { get; set; }
+        //public int IsActive { get; set; }
+        //public string CreatedBy { get; set; }
+        //public string UpdatedBy { get; set; }
+        //public DateTime CreatedDate { get; set;}
+        //public DateTime UpdatedDate { get; set; }
+    }
+}
